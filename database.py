e16dc41565550d74bfc4a7dfca889fae68dc2afc
Will <williamwsantosa@gmail.com>
2022-06-25 18:05:05 -0700

    Updated, created database functions

---
+++ database.py
@@ -0,0 +1,58 @@
+import sqlite3
+
+# Generate Database with discordid, elo, wins, games, primaryrole, and secondaryrole
+def generateDatabase():
+  '''
+  roles
+  0 = Fill
+  1 = Top
+  2 = Jungle 
+  3 = Mid
+  4 = Bot
+  5 = Support
+  '''
+  connection = sqlite3.connect("Data.db")
+  cursor = connection.cursor()
+  cursor.execute("CREATE TABLE Data (discordid BIGINT, elo INT, wins INT, games INT, primaryrole INT, secondaryrole INT)")
+  return connection
+
+# Insert new player into Database
+def insert(connection, discordid):
+  cursor = connection.cursor()
+  cursor.execute("INSERT INTO Data VALUES ({}, 1000, 0, 0, 0, 0)".format(discordid))
+  connection.commit()
+
+# Modify data
+def modify(connection, discordid, type, value):
+  cursor = connection.cursor()
+  cursor.execute("UPDATE Data SET {} = {} WHERE discordid = {}".format(type, value, discordid))
+
+# Read elo from Data
+def elo(connection, discordid):
+  cursor = connection.cursor()
+  data = cursor.execute("SELECT elo FROM Data WHERE discordid = {}".format(discordid)).fetchall()
+  return data[0][0]
+
+# Read wins from Data
+def wins(connection, discordid):
+  cursor = connection.cursor()
+  data = cursor.execute("SELECT wins FROM Data WHERE discordid = {}".format(discordid)).fetchall()
+  return data[0][0]
+
+# Read games from Data
+def games(connection, discordid):
+  cursor = connection.cursor()
+  data = cursor.execute("SELECT games FROM Data WHERE discordid = {}".format(discordid)).fetchall()
+  return data[0][0]
+
+# Read primaryrole from Data
+def primaryrole(connection, discordid):
+  cursor = connection.cursor()
+  data = cursor.execute("SELECT primaryrole FROM Data WHERE discordid = {}".format(discordid)).fetchall()
+  return data[0][0]
+
+# Read secondaryrole from Data
+def secondaryrole(connection, discordid):
+  cursor = connection.cursor()
+  data = cursor.execute("SELECT secondaryrole FROM Data WHERE discordid = {}".format(discordid)).fetchall()
+  return data[0][0]
--- main.py
+++ main.py
@@ -2,20 +2,64 @@ import discord
 import random
 import sys
 import os
+import math
+import sqlite3
 import config as cfg
-
+from database import *
 
 TOKEN = cfg.TOKEN
 client = discord.Client()
 
 ### Helper Functions
 
-# Returns a list of players on each team
-# If uneven, place random player into first team, otherwise second team
+# Returns the probability of r1 winning
+
+# Generate database with discordid, elo, wins, games, primaryrole, and secondaryrole
+def generateDatabase():
+  '''
+  roles
+  0 = Top
+  1 = Jungle 
+  2 = Mid
+  3 = Bot
+  4 = Support
+  '''
+  connection = sqlite3.connect("Data.db")
+  cursor = connection.cursor()
+  cursor.execute("CREATE TABLE Data (discordid BIGINT, elo INTEGER, wins INTEGER, games INTEGER, primaryrole INT, secondaryrole INT)")
+
+def prob(r1, r2, D):
+  return 1.0 / (1.0 + math.pow(10.0, (r2 - r1) / D))
+
+# Updates the Elo rating from results of the game
+def eloRating(r1, r2, D, K, w):
+  '''
+  r1          : Rating of player/team 1
+  r2          : Rating of player/team 2
+  D           : Effect of difference on probability to win or lose (higher = less effect)
+  K           : Maximum value a player's rating can change
+  w           : 1 or 2, player/team 1 wins or player/team 2 wins
+  returnValue : (newr1, newr2)
+  '''
+  p1, p2 = prob(r1, r2, D), prob(r2, r1, D)
+  rv1 = float(r1) + float(K) * (w - p1)
+  rv2 = float(r2) + float(K) * (w - p2)
+  return (int(rv1), int(rv2))
+
+# Returns the confidence level that a player is in their Elo
+def pConfidence(n):
+    '''
+    n           : Number of games played
+    returnValue : Confidence percent
+    '''
+    return 1.0 - math.pow(math.exp(1), -0.1 * n)
+
+# Returns teams with players
 def teamSort(players, n):
     '''
-    players  : List containing player names
-    n        : Number of teams
+    players     : List containing player names
+    n           : Number of teams
+    returnValue : {team# : [playerName]}
     '''
     # Create copy of players
     p_copy = players.copy()
@@ -35,9 +79,13 @@ def teamSort(players, n):
         
     return teams
 
-# Return a list of players in each team and their roles
-# Works only if there are 10 players
+# Returns teams with players and their roles
 def leagueSort(players):
+    '''
+    prereq      : len(players) == 10
+    players     : List containing player names
+    returnValue : ({playerName : role}, {playerName : role})
+    '''
     # Check if != 10 players
     if len(players) != 10:
         return
@@ -64,6 +112,11 @@ def leagueSort(players):
 @client.event
 async def on_ready():
     print("Logged in as {0}".format(client.user))
+
+# Return for channel messages
+@client.event
+async def on_message(message):
+    username = str(message.author).split("#")[0]
 
 # Run the client on server/machine
 if __name__ == "__main__":
