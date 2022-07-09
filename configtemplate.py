9f40058b4f32f32ce59262a34e42f9d58e3fec27
Will <williamwsantosa@gmail.com>
2022-07-09 00:30:32 -0700

    Updated, misspelling

--- configtemplate.py
+++ configtemplate.py
@@ -3,4 +3,4 @@
 TOKEN = "" # Insert discord bottoken here
 prefix = "!" # Insert prefix to use before command
 D = 400 # Determines how the difference in Elos affects Elo gain. The higher it is, the more Elo gained at the same difference.
-k = 32 # Maximum amount of Elo gained per game
+K = 32 # Maximum amount of Elo gained per game
